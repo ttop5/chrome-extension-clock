ded965a4e12f7f76b0c9ae3a7a95b5f2cb1467bb
何艳红 <heyh2@knownsec.com>
2016-11-17 18:27:19 +0800

    feat: chrome extension clock

---
+++ script.js
@@ -0,0 +1,13 @@
+function my_clock(el){
+  const today = new Date();
+  const h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  m = m>=10 ? m:('0'+m);
+  s = s>=10 ? s:('0'+s);
+  el.innerHTML = h + ":" + m + ":" + s;
+  setTimeout(function(){my_clock(el)}, 1000);
+}
+
+const clock_div = document.getElementById('clock_div');
+my_clock(clock_div);
